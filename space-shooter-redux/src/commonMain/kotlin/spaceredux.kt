be7f9c04fa4e5c62de29a8f5c95e7deedd0a9af1
soywiz <soywiz@gmail.com>
2020-10-17 23:21:09 +0200

    Some adjustments

--- space-shooter-redux/src/commonMain/kotlin/spaceredux.kt
+++ space-shooter-redux/src/commonMain/kotlin/spaceredux.kt
@@ -1,8 +1,14 @@
 package spaceredux
 
 import com.soywiz.korge.view.*
+import com.soywiz.korge.view.tiles.*
+import com.soywiz.korim.bitmap.*
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
+
+enum class SpaceShooterReduxBackground(val file: String) {
+	BLACK("black.png"), BLUE("blue.png"), DARK_PURPLE("darkPurple.png"), PURPLE("purple.png")
+}
 
 inline class SpaceShooterReduxView<T : View>(val genericView: View) {
 	@Suppress("UNCHECKED_CAST")
@@ -11,11 +17,9 @@ inline class SpaceShooterReduxView<T : View>(val genericView: View) {
 
 val <T : View> T.spaceShooterRedux get() = SpaceShooterReduxView<T>(this)
 
-//suspend fun SpaceShooterReduxView<out Container>.putBackground(): TileMap {
-suspend fun SpaceShooterReduxView<out Container>.putBackground(): Image {
-	return view.image(resourcesVfs["space-shooter-redux/bgs/blue.png"].readBitmap())
-	//val tileset = TileSet(mapOf(0 to resourcesVfs["space-shooter-redux/bgs/blue.png"].readBitmap().toBMP32().scaleLinear(0.5, 0.5).slice()))
-	//val tileset = TileSet(mapOf(0 to resourcesVfs["space-shooter-redux/bgs/blue.png"].readBitmap().slice()))
-	//val tilemap = view.tileMap(Bitmap32(1, 1), repeatX = TileMap.Repeat.REPEAT, repeatY = TileMap.Repeat.REPEAT, tileset = tileset)
-	//return tilemap
+suspend fun SpaceShooterReduxView<out Container>.putBackground(kind: SpaceShooterReduxBackground = SpaceShooterReduxBackground.BLACK): View {
+	val file = resourcesVfs["space-shooter-redux/bgs/${kind.file}"]
+	val tileset = TileSet(mapOf(0 to file.readBitmap().toBMP32().slice()))
+	val tilemap = view.tileMap(Bitmap32(1, 1), repeatX = TileMap.Repeat.REPEAT, repeatY = TileMap.Repeat.REPEAT, tileset = tileset)
+	return tilemap
 }
